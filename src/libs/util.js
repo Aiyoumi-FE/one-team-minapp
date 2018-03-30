68936deb188fc0eeb32188017231ec522312320a
孙学婧 <sunxj@aiyoumi.com>
2018-03-30 15:40:05 +0800

    eslint

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,15 +1,15 @@
 function checkEmail(value) {
-  let regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
-  return regEmail.test(value)
+    let regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+    return regEmail.test(value)
 }
 
 function checkPwd(value) {
     // let regPassword = /^[A-Za-z0-9]{8,16}$/
-    let regPassword = /^(?=.*\d)(?=.*[A-Za-z]).{8,16}$/g
+    let regPassword = /^(?=.*\d)(?=.*[A-Za-z]).{6,12}$/g
     return regPassword.test(value)
 }
 
 module.exports = {
-  checkEmail,
-  checkPwd
+    checkEmail,
+    checkPwd
 }
--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -2,9 +2,9 @@ import { ASYNC_INCREMENT } from '../types/counter'
 import { createAction } from 'redux-actions'
 
 export const asyncInc = createAction(ASYNC_INCREMENT, () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(1)
-    }, 1000)
-  })
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(1)
+        }, 1000)
+    })
 })
